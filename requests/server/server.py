5b35ccb095a5d5a6ef12553e0c8ceecae26836b0
PKrystian <krystian.pinczak2001@gmail.com>
2023-11-27 22:27:47 +0100

    t2, cleaning up directiory and added t2

---
+++ requests/server/server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+ 
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+ 
+ 
+@app.route("/")
+@cross_origin()
+def hello():
+    return {
+        "Status": "True"
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
