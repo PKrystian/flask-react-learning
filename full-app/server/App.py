5dc896234843d5e4db0f4304c45e15995295788d
PKrystian <krystian.pinczak2001@gmail.com>
2023-12-08 22:45:55 +0100

    full-app basic form on all ( main one ) http methods

--- full-app/server/App.py
+++ full-app/server/App.py
@@ -3,9 +3,36 @@ import params
 
 App = Flask(__name__)
 
-@App.route('/', methods=['GET'])
-def index():
-    return jsonify({'message': 'Hello, World!'})
+@App.route('/put', methods=['PUT'])
+def put():
+    return jsonify({'method': 'PUT'})
+@App.route('/get', methods=['GET'])
+def get():
+    return jsonify({'method': 'GET'})
+@App.route('/post', methods=['POST'])
+def post():
+    return jsonify({'method': 'POST'})
+@App.route('/delete', methods=['DELETE'])
+def delete():
+    return jsonify({'method': 'DELETE'})
+@App.route('/patch', methods=['PATCH'])
+def patch():
+    return jsonify({'method': 'PATCH'})
+@App.route('/options', methods=['OPTIONS'])
+def options():
+    return jsonify({'method': 'OPTIONS'})
+@App.route('/head', methods=['HEAD'])
+def head():
+    return jsonify({'method': 'HEAD'})
+@App.route('/trace', methods=['TRACE'])
+def trace():
+    return jsonify({'method': 'TRACE'})
+@App.route('/connect', methods=['CONNECT'])
+def connect():
+    return jsonify({'method': 'CONNECT'})
+@App.route('/any', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD', 'TRACE', 'CONNECT'])
+def any():
+    return jsonify({'method': 'ANY'})
 
 if __name__ == '__main__':
     App.run(debug=params.SERVER_DEBUG)
