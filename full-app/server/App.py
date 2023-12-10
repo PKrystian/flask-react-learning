92b66d313ddbfb4a9e38b07872e9872b9b0f0c9f
PKrystian <krystian.pinczak2001@gmail.com>
2023-12-10 19:09:35 +0100

    full-app, sql connection

--- full-app/server/App.py
+++ full-app/server/App.py
@@ -1,38 +1,34 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 import params
 
 App = Flask(__name__)
+App.config['SQLALCHEMY_DATABASE_URI'] = params.DATABASE_URI
+App.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(App)
 
-@App.route('/put', methods=['PUT'])
-def put():
-    return jsonify({'method': 'PUT'})
+# How to: create a table in the database
+# type flask shell, then:
+# 'db.create_all()'
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.Text())
+    date = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __init__(
+        self,
+        title: str,
+        body: str
+    ) -> None:
+        self.title = title
+        self.body = body
+        
+
 @App.route('/get', methods=['GET'])
 def get():
     return jsonify({'method': 'GET'})
-@App.route('/post', methods=['POST'])
-def post():
-    return jsonify({'method': 'POST'})
-@App.route('/delete', methods=['DELETE'])
-def delete():
-    return jsonify({'method': 'DELETE'})
-@App.route('/patch', methods=['PATCH'])
-def patch():
-    return jsonify({'method': 'PATCH'})
-@App.route('/options', methods=['OPTIONS'])
-def options():
-    return jsonify({'method': 'OPTIONS'})
-@App.route('/head', methods=['HEAD'])
-def head():
-    return jsonify({'method': 'HEAD'})
-@App.route('/trace', methods=['TRACE'])
-def trace():
-    return jsonify({'method': 'TRACE'})
-@App.route('/connect', methods=['CONNECT'])
-def connect():
-    return jsonify({'method': 'CONNECT'})
-@App.route('/any', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD', 'TRACE', 'CONNECT'])
-def any():
-    return jsonify({'method': 'ANY'})
 
 if __name__ == '__main__':
     App.run(debug=params.SERVER_DEBUG)
--- full-app/server/params.py
+++ full-app/server/params.py
@@ -1,3 +1,5 @@
-SERVER_HOST = ''
-SERVER_PORT = ''
+USERPASS = 'mysql://root:@'
+BASEDIR  = '127.0.0.1'
+DBNAME   = '/flask-learning-prod-1' # Change this to your database name
 SERVER_DEBUG = True # Set to FALSE for production
+DATABASE_URI = USERPASS + BASEDIR + DBNAME
